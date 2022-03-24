e2e9a28dc2d4cb8e84e3ef931739a23571278b2b
Ben <benymng@gmail.com>
2022-03-24 17:51:55 -0600

    changed back

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose');
 const routeAPI = require('./routes/api');
 
 let port = process.env.PORT || 5000;
-const uri = process.env.MONGODB_URI;
-const db = require(uri).mongoURI;
+const db = require('./config/key.js').mongoURI;
 mongoose
   .connect(db)
   .then(() => console.log('MongoDB connected...'))
