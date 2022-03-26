0382fc0a0fa895d5fea6f835777e6e577e8c4702
Ben <benymng@gmail.com>
2022-03-26 17:12:02 -0600

    added post and delete functionality

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const routeAPI = require('./routes/api');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 let port = process.env.PORT || 5000;
 const db = require('./config/key').mongoURI;
--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const router = express.Router();
 const Article = require('../models/article');
@@ -5,6 +6,33 @@ const Article = require('../models/article');
 router.get('/test', async (req, res) => {
   const articles = await Article.find();
   res.send(articles);
+});
+
+// get information for specific article
+router.get('/:id', async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  res.send(article);
+});
+
+router.post('/', async (req, res, next) => {
+  const article = new Article(req.body);
+  try {
+    await article.save();
+    res.send(article);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const article = await Article.findByIdAndDelete(req.params.id);
+
+    if (!article) response.status(404).send('No article found');
+    res.status(200).send();
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = router;
